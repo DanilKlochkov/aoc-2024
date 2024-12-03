31c9bdb296863492d31aacfded16736062201a9e
d.klochkov <d.klochkov@cloudcom.ru>
2024-12-03 20:18:37 +0300

    fix test template

--- tests/december03/solution_test.ts
+++ tests/december03/solution_test.ts
@@ -7,7 +7,7 @@ const secondData = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)und
 
 Deno.test({
     name: "Check first 03dec",
-    fn: async () => {
+    fn: () => {
         assertEquals(first(firstData), 161);
     }
 });
